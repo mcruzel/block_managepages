e4ebcb9a1befa20779fadc93d2ed2f114f74004c
Maxime Cruzel <84211047+mcruzel@users.noreply.github.com>
2025-06-08 00:55:18 +0200

    Rename privacy provider method

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -11,7 +11,7 @@ namespace block_managepages\privacy;
 defined('MOODLE_INTERNAL') || die();
 
 class provider implements \core_privacy\local\metadata\null_provider {
-    public static function _get_reason() {
+    public static function get_reason() {
         return get_string('privacy:metadata', 'block_managepages');
     }
 }
